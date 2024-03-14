9f104d5834fe57d6aa55ed81dad398119be854f9
jacobyi0730 <jacobyi0730@gmail.com>
2024-03-14 12:11:24 +0900

    방생성 기능구현
    방생성 기능구현

--- Source/NetTPSCD/Public/NetGameInstance.h
+++ Source/NetTPSCD/Public/NetGameInstance.h
@@ -1,9 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "NetGameInstance.generated.h"
 
 /**
@@ -13,5 +14,22 @@ UCLASS()
 class NETTPSCD_API UNetGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	// 시작할 때 세션인터페이스를 기억하고싶다.
+	virtual void Init() override;
+
+	IOnlineSessionPtr sessionInterface;
+
+	// 세션(session) == 방(room)
+	// 세션생성요청함수
+	FString hostName = TEXT("Jacob");
+	void CreateRoom(int32 maxPlayerCount, FString roomName);
+	// 세션생성응답함수
+	void OnMyCreateRoomComplete( FName sessionName , bool bWasSuccessful );
+
+
+	// 세션검색요청
+	// 세션검색응답
 	
 };
